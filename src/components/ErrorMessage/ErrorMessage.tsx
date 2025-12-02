814e28d593dca01e45070926159049fb94d145a3
Halynka Obykhvost <fiksikiob@gmail.com>
2025-12-02 15:54:12 +0200

    add markup

--- src/components/ErrorMessage/ErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-import css from './ErrorMessage.module.css';
-
-export default function ErrorMessage() {
-  return (
-    <p className={css.text}>
-      Oops, something went wrong. Please try again later
-    </p>
-  );
-}
